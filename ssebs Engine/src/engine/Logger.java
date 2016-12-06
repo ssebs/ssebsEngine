588e8a50b833764455abc94b5b2be5f343caf854
Sebastian Safari <ssebs@ymail.com>
2016-12-06 19:56:32 +0000

    Corrected files, pushed wrong dir

---
+++ ssebs Engine/src/engine/Logger.java
@@ -0,0 +1,53 @@
+package engine;
+
+import java.awt.Font;
+import java.util.ArrayList;
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.TrueTypeFont;
+
+public class Logger 
+{
+	TrueTypeFont _font;
+	
+	/**
+	 * Logger Constructor, this class will display strings on the display
+	 * @param awtFont
+	 */
+	public Logger(Font awtFont)
+	{		
+		_font = new TrueTypeFont(awtFont, true);
+	}
+	
+	
+	public void log(String logTxt)
+	{
+		_font.drawString(10, 10, logTxt, Color.black);
+	}
+	
+	public void log(String logTxt, Color c)
+	{
+		_font.drawString(10, 10, logTxt, c);
+	}
+	
+	public void log(String logTxt, int yOff)
+	{
+		_font.drawString(10, 10+yOff, logTxt, Color.black);
+	}
+	
+	public void log(String logTxt, int yOff, Color c)
+	{
+		_font.drawString(10, 10+yOff, logTxt, c);
+	}
+	
+	public void log(String logTxt, int x, int y)
+	{
+		_font.drawString(x, y, logTxt, Color.black);
+	}
+	
+	public void log(String logTxt, int x, int y, Color c)
+	{
+		_font.drawString(x, y, logTxt, c);
+	}
+	
+}
--- ssebs Engine/src/tester/Tester.java
+++ ssebs Engine/src/tester/Tester.java
@@ -1,10 +1,18 @@
 package tester;
 
+import java.awt.Font;
+import java.io.InputStream;
+
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
+import org.newdawn.slick.Color;
+import org.newdawn.slick.TrueTypeFont;
 import org.newdawn.slick.opengl.Texture;
+import org.newdawn.slick.util.ResourceLoader;
 
 import engine.AudioHelper;
+import engine.Logger;
 import engine.LogicHelper;
 import engine.RenderHelper;
 import engine.URLDataHelper;
@@ -13,18 +21,39 @@ public class Tester
 {
 	private static Texture backgroundTexture, dargonTexture;
 
-	private static float x = 1280 / 2 - 128;
-	private static float y = 720 / 2 - 32;
+	private static float _x = 1280 / 2 - 128;
+	private static float _y = 720 / 2 - 32;
 
+	private static Logger _logger;
+	
 	public static void main(String[] args)
 	{
 		RenderHelper.createDisplay(1280, 720, "ssebs Engine");
 		AudioHelper.createAL();
-		AudioHelper.playSound("Whoosh.wav");
+		//AudioHelper.playSound("Whoosh.wav");
 
 		backgroundTexture = RenderHelper.loadTexture(backgroundTexture, "res/BackgroundTexture.png");
 		dargonTexture = RenderHelper.loadTexture(dargonTexture, "res/Dargon.png");
 
+		try 
+		{
+			InputStream is = ResourceLoader.getResourceAsStream("res/fonts/UbuntuMono-R.ttf");
+			Font fnt = Font.createFont(Font.TRUETYPE_FONT, is);
+			fnt = fnt.deriveFont(16f);
+			_logger = new Logger(fnt);
+			System.out.println("Ubuntu");			
+		} catch (Exception e) 
+		{
+			e.printStackTrace();
+			_logger = new Logger(new Font("Comic Sans MS", Font.BOLD, 24));
+			System.out.println("Comic");
+		}
+		
+		
+		/**
+		 * FIX FONTS
+		**/	
+		
 		while (!Display.isCloseRequested())
 		{
 			long delta = (long) LogicHelper.getDelta();
@@ -41,29 +70,41 @@ public class Tester
 
 	private static void gameplay(long delta)
 	{
-		RenderHelper.texRender(dargonTexture, x, y);
+		RenderHelper.texRender(dargonTexture, _x, _y);
 
 		if (Keyboard.isKeyDown(Keyboard.KEY_LEFT))
 		{
-			x -= delta / 1.25;
+			_x -= delta / 1.25;
 		}
 		if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT))
 		{
-			x += delta / 1.25;
+			_x += delta / 1.25;
 		}
 		if (Keyboard.isKeyDown(Keyboard.KEY_UP))
 		{
-			y -= delta / 1.25;
+			_y -= delta / 1.25;
 		}
 		if (Keyboard.isKeyDown(Keyboard.KEY_DOWN))
 		{
-			y += delta / 1.25;
+			_y += delta / 1.25;
 		}
 
 		if (Keyboard.isKeyDown(Keyboard.KEY_RETURN))
 		{
 			System.out.println(URLDataHelper.getStringDataAsString("https://raw.githubusercontent.com/ssebs/ssebsEngine/master/ssebs%20Engine/Misc/serverTestFile.txt"));
 		}
+		
+		if (Mouse.isButtonDown(0))
+		{
+			String coords = "X: " + Mouse.getX() + ", Y: " + Mouse.getY();
+			_logger.log("Left Mouse Click--" + coords);
+		}
+		if (Mouse.isButtonDown(1))
+		{
+			String coords = "X: " + Mouse.getX() + ", Y: " + Mouse.getY();
+			_logger.log("Right Mouse Click--" + coords, 15);
+		}
 
 	}
+	
 }
